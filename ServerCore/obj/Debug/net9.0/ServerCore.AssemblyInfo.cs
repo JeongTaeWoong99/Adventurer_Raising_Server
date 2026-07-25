14042333fa775b598beedf912d65fa96151402f6
JeongTaeWoong99 <jto0402@naver.com>
2026-07-25 10:08:37 +0900

    리드미 수정

--- ServerCore/obj/Debug/net9.0/ServerCore.AssemblyInfo.cs
+++ ServerCore/obj/Debug/net9.0/ServerCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e211828e12d05582fc8b3a6e52ab47cc552b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a5800d87984727577d7e0e3206c99adc823d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
